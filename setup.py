e3cc39322b4dd6710048c3d0b2036823c1f21f7f
Ralph Bean <ralph.bean@gmail.com>
2011-12-14 21:43:45 -0500

    License and trove classifiers.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1.2'
+version = '0.1.3'
 
 f = open('README.rst')
 long_description = f.read().strip()
@@ -12,18 +12,21 @@ setup(name='taskw',
       version=version,
       description="Python bindings for your taskwarrior database",
       long_description=long_description,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Programming Language :: Python :: 2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Intended Audience :: Developers",
+      ],
       keywords='taskwarrior task',
       author='Ralph Bean',
       author_email='ralph.bean@gmail.com',
       url='http://github.com/ralphbean/taskw',
-      license='',
+      license='GPLv3+',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
+      install_requires=[],
       entry_points="""
       # -*- Entry points: -*-
       """,
