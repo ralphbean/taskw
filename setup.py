4e605403c6bc750ec1c330237b77b3f162536d8f
Ralph Bean <rbean@redhat.com>
2014-08-23 21:34:04 -0400

    Turns out unittest2 is a backport from py2.7, not from py3.x.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ tests_require = [
     'nose',
 ]
 
-if sys.version_info < (3, ):
+if sys.version_info < (2, 7):
     tests_require.append('unittest2')
 
 if sys.version_info[0] == 2 and sys.version_info[1] < 7:
--- taskw/test/test_taskrc.py
+++ taskw/test/test_taskrc.py
@@ -5,7 +5,7 @@ from taskw.taskrc import TaskRc
 from taskw.fields import NumericField, ChoiceField
 
 
-if sys.version_info >= (3, ):
+if sys.version_info >= (2, 7):
     from unittest import TestCase
 else:
     from unittest2 import TestCase
