91cc20f96b50a4ebe72c22fb3b498b9b3e8a97f9
Ralph Bean <rbean@redhat.com>
2015-09-10 12:41:29 -0400

    task-2.5.0.beta1 stuff.

--- taskw/fields/commaseparateduuid.py
+++ taskw/fields/commaseparateduuid.py
@@ -1,19 +1,39 @@
 from __future__ import absolute_import
 
+from distutils.version import LooseVersion
+
 import uuid
 
 from .base import DirtyableList, Field
 
 
 class CommaSeparatedUUIDField(Field):
+    version = LooseVersion('2.4')
+
     def deserialize(self, value):
         if not value:
             return DirtyableList([])
-        return DirtyableList([uuid.UUID(v) for v in value.split(',')])
+
+        # In task-2.5, this moved from a comma-separated string to a real list.
+        # here we allow a migration path where old splitable strings are
+        # handled as well as newschool lists.
+        if hasattr(value, 'split'):
+            value = value.split(',')
+
+        return DirtyableList([uuid.UUID(v) for v in value])
 
     def serialize(self, value):
         if not value:
             value = []
+
         if not hasattr(value, '__iter__'):
             raise ValueError("Value must be list or tuple, not %r." % value)
-        return ','.join([str(v) for v in value])
+
+        if self.version < LooseVersion('2.5'):
+            return ','.join([str(v) for v in value])
+        else:
+            # We never hit this second code branch now.  taskwarrior changed
+            # API slightly in version 2.5, but we're just going to go with
+            # backwards compatibility for now.
+            # Some day we should switch wholesale to the new path.
+            return [str(v) for v in value]
