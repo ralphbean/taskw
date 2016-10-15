8406132884d6de589e9c90dbd4b70c95918caf73
Grégoire Détrez <gregoire@fripost.org>
2016-10-15 18:34:04 +0200

    Add a function to clean control characters
    It removes all control characters from a binary string ('bytes' in python
    3) except the ones that are printable-ish that we might want to keep
    around ("\t\n\v\f\r").
    
    Also adds unit tests for the new function

--- taskw/test/test_utils.py
+++ taskw/test/test_utils.py
@@ -11,7 +11,8 @@ from taskw.utils import (
     decode_task,
     encode_task,
     encode_task_experimental,
-    DATE_FORMAT
+    DATE_FORMAT,
+    clean_ctrl_chars,
 )
 
 TASK = {'description': "task 2 http://www.google.com/",
@@ -200,3 +201,14 @@ class TestUtils(object):
         actual_overrides = convert_dict_to_override_args(overrides)
 
         eq_(set(actual_overrides), set(expected_overrides))
+
+
+class TestCleanExecArg(object):
+    def test_clean_null(self):
+        eq_(b"", clean_ctrl_chars(b"\x00"))
+
+    def test_all_ctrl_chars(self):
+        """ Test that most (but not all) control characters are removed """
+        # input = bytes(range(0x20))
+        input = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f' # For python 2 compatibility
+        eq_(b"\t\n\v\f\r", clean_ctrl_chars(input))
