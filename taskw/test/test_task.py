2d70de7b16da3bd7241040c694b83b15f6fd8cf0
Fongshway <kmf78@georgetown.edu>
2018-10-27 16:09:32 -0700

    Update imports

--- taskw/test/test_task.py
+++ taskw/test/test_task.py
@@ -1,18 +1,13 @@
 import copy
 import datetime
-import sys
 import uuid
+from unittest import TestCase
 
 import pytz
 import six
 from dateutil.tz import tzutc
 
 from taskw.task import Task
-
-if sys.version_info >= (2, 7):
-    from unittest import TestCase
-else:
-    from unittest2 import TestCase
 
 
 class TestTaskDirtyability(TestCase):
