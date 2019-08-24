0cdd1765f281e900ca7116fbb55f1520ca013e21
Nikos Koukis <nickkouk@gmail.com>
2019-08-24 21:20:01 +0100

    Fix bug on deserialization of float (urgency) and in marshalling mode

--- taskw/fields/numeric.py
+++ taskw/fields/numeric.py
@@ -13,14 +13,21 @@ class NumericField(Field):
     def deserialize(self, value):
         if value is None:
             return value
-        try:
-            return int(value)
-        except ValueError:
-            pass
-        try:
-            return float(value)
-        except ValueError:
-            pass
+        elif isinstance(value, str):
+            try:
+                return int(value)
+            except ValueError:
+                pass
+            try:
+                return float(value)
+            except ValueError:
+                pass
+        elif isinstance(value, int) or isinstance(value, float):
+            # already desialized
+            return value
+        else:
+            raise ValueError("Unhandled type [{}] passed during deserialization for value [{}]"
+                             .format(type(value), value))
 
         # If we've made it this far, somehow Taskwarrior has
         # a non-numeric value stored in this field; this shouldn't
