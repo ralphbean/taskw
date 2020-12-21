2c78af798c4844c07b442abd62d4277ce76a6012
Edward Betts <edward@4angle.com>
2020-12-21 21:34:15 +0000

    verbose must be first param for taskwarrior 2.5.2
    There is a bug in version of taskwarrior that the rc.verbose=new-uuid
    argument will only work if it is the first argument.
    
    https://github.com/GothenburgBitFactory/taskwarrior/issues/1953
    
    This change moves the 'verbose' config option to be first in
    DEFAULT_CONFIG_OVERRIDES.
    
    Without this change the test suite fails and it isn't possible to create
    new tasks.

--- taskw/warrior.py
+++ taskw/warrior.py
@@ -412,10 +412,10 @@ class TaskWarriorShellout(TaskWarriorBase):
     and https://github.com/ralphbean/taskw/issues/30 for more.
     """
     DEFAULT_CONFIG_OVERRIDES = {
+        'verbose': 'nothing',
         'json': {
             'array': 'TRUE'
         },
-        'verbose': 'nothing',
         'confirmation': 'no',
         'dependency': {
             'confirmation': 'no',
