83c5521bd6aa94a3405d17fedf5f3947b01cf330
Vladimir Rusinov <vladimir.rusinov@gmail.com>
2021-05-23 18:37:46 +0100

    Fix .tox_build_taskwarrior.sh.
    It contained outdated repository path and was actually failing. Due to
    lack of `set -e` failures were not noticed as tests were just using
    `task` from the system.

--- .tox_build_taskwarrior.sh
+++ .tox_build_taskwarrior.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
-if [ ! -d "$1/task" ]; then
-    git clone https://git.tasktools.org/scm/tm/task.git $1/task
+
+set -e
+
+TW_GIT_REPO="https://github.com/GothenburgBitFactory/taskwarrior.git"
+
+if [ -z "$1" ]; then
+  echo "envdir not specified"
+  echo 'Usage: .tox_build_taskwarrior.sh "{envdir}" "{toxinidir}"'
+  exit 1
+fi
+
+if [ ! -x "$1/bin/task" ]; then
+    rm -rf "${1?}/task"
+    # --branch is misleading - it also accepts tags
+    # So we can do a shallow checkout of a specific tag:
+    git clone --depth 1 "${TW_GIT_REPO?}" $1/task --branch ${TASKWARRIOR?}
     cd $1/task
-    git checkout $TASKWARRIOR
     # Use the 'release build' just to make things faster
-    cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX:PATH=$1 .
-    make
+    # Note about prefix: we are in {envdir}/task, so our install prefix
+    # is out level up (thus '..').
+    cmake -DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX:PATH=.. .
+    # Run parralell build as majority of environments these days would have at
+    # least two CPUs.
+    make -j2
     make install
     cd $2
 fi
